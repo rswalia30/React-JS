2cba171864ff3b1f41ca199d9e6ca5bacdf7e337
Rohit <2020010006264@gndu.ac.in>
2024-08-05 03:15:22 +0530

    initial commit

--- Food-Ordering-App/src/Components/Body.jsx
+++ Food-Ordering-App/src/Components/Body.jsx
@@ -8,7 +8,7 @@ export const Body = () => {
 
     /******************************** useState() Hook ************************************************ */
     // Normal JS variable
-    let list2 = [
+    let normalList = [
         {
             id: 101,
             name: "Chaska Cafe",
@@ -24,49 +24,13 @@ export const Body = () => {
             time: "20-25",
             cusine: ["Biscuits", "Ice-Cream", "Chinese"],
             stars: "4.6",
-        },
-        {
-            id: 103,
-            name: "Meaow Momo2 ",
-            img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/76a05b39545d5a2bf80d6a3e3e46544a",
-            time: "35-40",
-            cusine: ["Cafe", "Italian", "Chinese"],
-            stars: "4.9",
-        },
+        }
     ];
 
     // State Variable -> super powerful variable
-    // const [list] = useState([]);             // Default Value : empty list
+    // const [list, setList] = useState([]);             // Default Value : empty list
 
-    // const [list, setList] = useState([
-    //     {
-    //         id: 101,
-    //         name: "Chaska Cafe",
-    //         img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/uxgpy3bwmc5rsojmtw43",
-    //         time: "60-65",
-    //         cusine: ["Cafe", "Italian", "Chinese"],
-    //         stars: "3.8",
-    //     },
-    //     {
-    //         id: 102,
-    //         name: "Gupta Bakery",
-    //         img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/76a05b39545d5a2bf80d6a3e3e46544a",
-    //         time: "20-25",
-    //         cusine: ["Biscuits", "Ice-Cream", "Chinese"],
-    //         stars: "4.6",
-    //     },
-    //     {
-    //         id: 103,
-    //         name: "Wow Momo",
-    //         img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/crncfqmsuosriqcrp9ez",
-    //         time: "35-40",
-    //         cusine: ["Cafe", "Italian", "Chinese"],
-    //         stars: "4.9",
-    //     },
-    // ]);
-
-    // const [list, setList] = useState([]);
-    const [list, setList] = useState(list2);
+    const [list, setList] = useState(normalList);
 
     // Array Destructuring
     // const arr = useState(list2);
@@ -79,10 +43,7 @@ export const Body = () => {
     // console.log("Body is going to render");
 
     // Do something after Rendering the Component
-    useEffect(() => {
-        // console.log("After Rendering Component");
-        fetchDataFromAPI();
-    }, []);
+    useEffect(() => { fetchDataFromAPI() }, []);
 
     const fetchDataFromAPI = async () => {
         const data = await fetch("http://localhost:8080/res");  // SpringBoot
@@ -104,9 +65,7 @@ export const Body = () => {
                     const filteredList = list.filter(
                         (obj) => obj.stars > 4
                     );
-                    // It will re-render our Component
                     setList(filteredList);
-                    // console.log(filteredList);
                 }}
             >
                 Top Rated Restraunts
@@ -143,5 +102,5 @@ export const Body = () => {
                 }
             </div>
         </div>
-    )
+    );
 }
