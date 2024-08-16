3aa7a299a8ae93c7dfa32e01c096e258cda304ec
Rohit <2020010006264@gndu.ac.in>
2024-08-16 12:25:44 +0530

    update api

--- Swiggy/src/utils/constants.jsx
+++ Swiggy/src/utils/constants.jsx
@@ -3,6 +3,8 @@ export const SWIGGY_LOGO =
 
 export const SWIGGY_API =
     "https://www.swiggy.com/mapi/homepage/getCards?lat=31.661445&lng=74.8852469";
+// "https://www.swiggy.com/mapi/homepage/getCards?lat=31.6339793&lng=74.8722642";
+// "https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.99740&lng=79.00110&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
 export const SWIGGY_CLOUDINARY_IMG =
     "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_366/";
